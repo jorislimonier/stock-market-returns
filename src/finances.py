2b141b7a3801e4d148c48bfad5b2d91e49fc1ccc
jorislimonier <joris.limonier@hotmail.fr>
2022-12-28 22:07:47 +0100

    Migrate code logiv to python file

--- src/finances.py
+++ src/finances.py
@@ -1,11 +1,15 @@
 from typing import Union
-import plotly.express as px
-import plotly.graph_objects as go
+
 import numpy as np
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.io as pio
+
+pio.templates.default = "plotly_white"
 
 
-class Returns:
+class Finances:
   def __init__(
     self,
     initial_inv: Union[float, int],
@@ -13,14 +17,18 @@ class Returns:
     rate: float,
     n_years: Union[float, int],
   ) -> None:
+    # Passed parameters
     self.initial_inv = initial_inv
     self.monthly_inv = monthly_inv
     self.rate = rate
     self.n_years = n_years
+
+    # Computed parameters
     self.monthly_factor = 1 + rate / 12
     self.n_months = 12 * n_years
 
     self._compute_finances()
+    self._compute_yearly_data()
 
   def _compute_finances(self) -> None:
     """
@@ -62,3 +70,39 @@ class Returns:
 
     # Compute total returns after `n_years`
     self.total_return = self.finances.iloc[-1]["returns"]
+
+  def _compute_yearly_data(self) -> None:
+    is_complete_year = [m for m in range(len(self.finances)) if m % 12 == 0]
+    yearly_finances = self.finances.loc[is_complete_year]
+    yearly_finances["returns"] = yearly_finances["returns"].astype(int)
+    yearly_finances["gain"] = yearly_finances["returns"] - yearly_finances["investments"]
+    yearly_finances["yearly_returns"] = yearly_finances["returns"].diff().fillna(0).astype(int)
+    self.yearly_finances = yearly_finances
+
+  def plot_investments_vs_returns(self):
+
+    var_names = ["monthly inv", "rate", "number of years", "total inv", "total return"]
+    var_used = [
+      f"{self.monthly_inv}€",
+      f"{self.rate:.0%}",
+      f"{self.n_years}",
+      f"{self.total_invested:,.0f}€",
+      f"{self.total_return:,.0f}€",
+    ]
+
+    title = ",   ".join([f"{name}: {v}" for name, v in zip(var_names, var_used)])
+
+    fig = (
+      px.line(
+        data_frame=self.finances,
+        x="n_years",
+        y=["investments", "returns"],
+        log_y=False,
+        title=title,
+        hover_data=["n_years"],
+      )
+      .update_xaxes(title="Years")
+      .update_yaxes(title="Total return")
+    )
+
+    return fig
